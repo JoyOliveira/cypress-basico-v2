f6320a29f3d1030cb02fd5844bcd9f2cf9576399
JoyOliveira <joyce.lvr@gmail.com>
2025-05-28 22:24:52 -0300

    breaking project

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 let isPhoneRequired = false
 
-const phoneLabelSpan = document.querySelector('.phone-label-span')
+const phoneLabelSpan = Mocument.querySelector('.phone-label-span')
 const phoneField = document.getElementById('phone')
 
 document.querySelector('#phone-checkbox')
